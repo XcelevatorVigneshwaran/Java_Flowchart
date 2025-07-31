5f9bcfd670e1163da5a1af97c05ca197fc8a6dd4
XcelevatorVigneshwaran <vigneshwaran.2526@xcelevate.org>
2025-07-31 11:37:57 +0530

    Add files via upload

---
+++ FCP014.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class FCP014
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b=scan.nextInt();
+int c=scan.nextInt();
+if(a>b)
+{
+if(a>c)
+{
+System.out.println(a);
+}
+else
+{
+System.out.println(c);
+}
+}
+else if(b>c)
+{
+System.out.println(b);
+}
+else
+{
+System.out.println(c);
+}
+}
+}
