31aa723fb0f9b2cbe986f593bd7bdc4c5b27bf48
XcelevatorVigneshwaran <vigneshwaran.2526@xcelevate.org>
2025-07-31 11:35:02 +0530

    Add files via upload

---
+++ FCP007.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class FCP007
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+double c=scan.nextDouble();
+int f=(int)((9*c)/5+32);
+System.out.println(f);
+}
+}
