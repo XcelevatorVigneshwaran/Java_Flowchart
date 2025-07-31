968c8b8e793ec16528929b0ea131f888f51aa1a4
XcelevatorVigneshwaran <vigneshwaran.2526@xcelevate.org>
2025-07-31 11:35:29 +0530

    Add files via upload

---
+++ FCP008.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FCP008
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int num1=scan.nextInt();
+int num2=scan.nextInt();
+num1=num1+num2;
+num2=num1-num2;
+num1=num1-num2;
+System.out.println(num1);
+System.out.println(num2);
+}
+}
